4197481d893b672aa5667465e4bb4eaad25c7735
Harvey Wheaton <harvey.wheaton@codeclan.com>
2016-02-16 19:21:50 +0000

    Initial commit of both the function and test files

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,86 @@
+/A library of functions from Cohort 1 Day 2 that perform a varety of functions (see comments within each function for details of each one).  This library is tested by the test suite contained in ruby_functions_spec.rb and is a demonstration of TDD. HW/
+
+
+/ return_10 - returns a fixednum 10/
+def return_10()
+  return(10)
+end
+
+/ add - exects two numbers, returns the sum/
+def add(a,b)
+  return a+b
+end
+
+/subtract - expects two numbers a and b, returns a minus b/
+def subtract(a,b)
+  return a-b
+end
+
+/multiply - expects two numbers a and b, returns a times b/
+def multiply(a,b)
+  return a*b
+end
+
+/divide - expects two numbers a and b, returns a divided by b/
+def divide(a,b)
+  return a/b
+end
+
+/length_of_string - returns the length of a single string passed to the function/
+def length_of_string(string)
+  return string.length
+end
+
+/join_string - returns concatenation of the two passed strings/
+def join_string(str1,str2)
+  return str1+str2
+end
+
+/add_string_as_number - takes two strings, converts to a number and returns the sum as a number/
+def add_string_as_number(str1,str2)
+  return str1.to_i+str2.to_i
+end
+
+/takes a number 1-12 and returns the full month name/
+def number_to_full_month_name(month_number)
+  months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+  return months.at(month_number-1)
+end
+/takes a number 1-12 and returns the 3 shar short month name/
+def number_to_short_month_name(month_number)
+  short_months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+  return short_months.at(month_number-1)
+end
+
+/returns the volume of a cube given the edge length/
+def volume_of_cube(cube_length)
+  return cube_length**cube_length
+end
+
+/volume_of_sphere - returns the volume of a sphere when passed the radius/
+def volume_of_sphere(radius)
+  return (radius**radius)*3.14
+end
+
+/days_until-christmas - returns days until Xmas of the CURRENT year from the date passed as a parameter/
+def days_until_christmas(start_date)
+
+  current_date = Time.new
+  current_year=current_date.year
+
+  xmas = Date.new(current_year.to_i,12,25)
+
+  return (xmas-start_date).to_i
+
+end
+
+/age_of_person - takes a date as a parameter and returns the persons age in years/
+def age_of_person(birth_date)
+  current_year = Time.new.year.to_int
+  birth_year = birth_date.year.to_int
+  return current_year-birth_year
+
+end
+
+
+
---
+++ specs/ruby_functions_spec.rb
@@ -0,0 +1,106 @@
+/Test suite for ruby_functions_practice - contains at least one test per function contained in the ruby_functions_practice.rb library/
+
+require( 'minitest/autorun' )
+require_relative( '../ruby_functions_practice' )
+
+/I think I could have done this with the Time class (as used in the test suite) but I initially started by incuding the 'date' class from Google research):/
+require 'date'
+
+class Functions_Practice < MiniTest::Test
+
+  def test_return_10()
+    return_10_result = return_10()
+    assert_equal( 10, return_10_result )
+  end
+
+  def test_add()
+    add_result = add( 1, 2 )
+    assert_equal( 3, add_result )
+  end
+
+  def test_subtract()
+    subtract_result = subtract( 10, 5 )
+    assert_equal( 5, subtract_result )
+  end
+
+  def test_multiply()
+    multiply_result = multiply( 4, 2 )
+    assert_equal( 8, multiply_result )
+  end
+
+  def test_divide()
+    divide_result = divide( 10, 2 )
+    assert_equal( 5, divide_result )
+  end
+
+  def test_length_of_string()
+    test_string = "A string of length 21"
+    length_of_string = length_of_string( test_string )
+    assert_equal( 21, length_of_string )
+  end
+
+  def test_join_string()
+    string_1 = "Mary had a little lamb, "
+    string_2 = "it's fleece was white as snow"
+    joined_string = join_string( string_1, string_2 )
+    assert_equal( "Mary had a little lamb, it's fleece was white as snow", joined_string )
+  end
+
+  def test_add_string_as_number()
+    add_result = add_string_as_number( "1", "2" )
+    assert_equal( 3, add_result )
+  end
+
+  def test_number_to_full_name()
+    first_month_string = number_to_full_month_name( 1 )
+    third_month_string = number_to_full_month_name( 3 )
+    ninth_month_string = number_to_full_month_name( 9 )
+    assert_equal( "January", first_month_string )
+    assert_equal( "March", third_month_string ) 
+    assert_equal( "September", ninth_month_string )
+  end
+
+  def test_number_to_short_month_name()
+    first_month_string = number_to_short_month_name( 1 )
+    third_month_string = number_to_short_month_name( 3 )
+    ninth_month_string = number_to_short_month_name( 9 )
+    assert_equal( "Jan", first_month_string )
+    assert_equal( "Mar", third_month_string )
+    assert_equal( "Sep", ninth_month_string )
+  end
+
+  #Further
+
+  #Given the length of a side of a cube calculate the volume 
+  def test_volume_of_cube()
+    cube_volume = volume_of_cube(3)
+    assert_equal(27,cube_volume)
+  end
+
+  #Given the radius of a sphere calculate the volume
+  def test_volume_of_sphere()
+    sphere_volume = volume_of_sphere(10)
+    assert_equal(31400000000.0,sphere_volume)
+  end
+
+  #http://ruby-doc.org/stdlib-2.1.1/libdoc/date/rdoc/Date.html
+  #Days until christmas, Calculate how many nights there are from today until Christmas morning
+  def test_days_until_christmas()
+      /test runs with two defined dates in different years/
+      days_to_xmas=days_until_christmas(Date.parse('20th Oct 2016'))
+      assert_equal(66,days_to_xmas)
+      days_to_xmas=days_until_christmas(Date.parse('1st Jan 2015'))
+      assert_equal(724,days_to_xmas)
+
+  end
+
+  #Given a date of birth, calculate how old a person born on that date would be
+  def test_age_of_person()
+/test runs with two defined dates of birth/
+    age=age_of_person(Date.parse('4th Jan 1966'))
+    assert_equal(50,age)
+
+  end
+
+
+end
